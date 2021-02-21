07d347eaf1b2c85515c6d497b3f16e0e8dd95757
atong2023 <atong2023@k12.andoverma.us>
2021-02-21 11:03:39 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://atong2023:<jn5HavjpCCwKoMk0>@cluster0.vlota.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoConnectionURL = "mongodb+srv://atong2023:jn5HavjpCCwKoMk0@cluster0.vlota.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
 const databaseName = "test";
 
